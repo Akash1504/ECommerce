35e6fc3972cd34697bd1f6176ad74f7ef505dffb
Akash1504 <73223006+Akash1504@users.noreply.github.com>
2020-10-21 19:02:08 +0530

    Add files via upload

---
+++ product.java
@@ -0,0 +1,31 @@
+package Shopping;
+
+public class product 
+{
+	private String prodName;
+	private double price;
+	private int pId;
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getpId() {
+		return pId;
+	}
+	public void setpId(int pId) {
+		this.pId = pId;
+	}
+	
+	
+	
+	
+	
+}
